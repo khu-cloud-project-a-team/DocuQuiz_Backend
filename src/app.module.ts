159d87428a135e7715fb46d8d4f83890ee772683
pjh0846 <pjh846@naver.com>
2025-12-09 18:51:09 +0900

    Add SSL configuration to TypeORM for PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,10 +20,13 @@ import { UserModule } from './user/user.module';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
+        port: 5432,
         username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
